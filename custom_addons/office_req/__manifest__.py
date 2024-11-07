af6bce75143b0cdb739a6812e6dd8a39701d498b
Mark kimaaathi <markangelkimathi@gmail.com>
2024-11-07 21:58:45 +0300

    Manifest

---
+++ custom_addons/office_req/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Office Requests',
+    'version': '1.0.0',
+    'summary': 'Make office request',
+    'description': """
+        Office Requests 
+    """,
+    'author': 'Mark Kimathi',
+    'category': 'Tools,Office',
+    "license": 'LGPL-3',
+    'depends': ['base', 'hr'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+
+}
