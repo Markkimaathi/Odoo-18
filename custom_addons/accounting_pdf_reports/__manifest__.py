a009235d0f0e6d0fc492ed92ddc596e64fbde7ad
Mark kimaaathi <markangelkimathi@gmail.com>
2024-11-27 18:55:58 +0300

    Manifest

---
+++ custom_addons/accounting_pdf_reports/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo 18 Accounting Financial Reports',
+    'version': '1.2',
+    'category': 'Invoicing Management',
+    'description': 'Accounting Reports For Odoo 18, Accounting Financial Reports, '
+                   'Odoo 18 Financial Reports',
+    'summary': 'Accounting Reports For Odoo 18',
+    'sequence': '1',
+    'author': 'Mark ',
+    'license': 'LGPL-3',
+    'depends': ['account'],
+    'data': [
+    ],
+    'pre_init_hook': '_pre_init_clean_m2m_models',
+    'images': ['static/description/banner.gif'],
+}
+
