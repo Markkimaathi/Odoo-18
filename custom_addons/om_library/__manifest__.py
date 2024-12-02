b49689ee7e49f81f46740d50e08378b00bbf50a3
Mark kimaaathi <markangelkimathi@gmail.com>
2024-12-02 11:54:09 +0300

    Manifest

---
+++ custom_addons/om_library/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Library Management',
+    'version': '1.0',
+    'summary': 'A Simple library management system',
+    'description': """
+       Manage books, loans, etc
+    """,
+    'category': 'Management,Books',
+    "author": "Mark Kimathi",
+    "license": 'LGPL-3',
+    'depends': ['base', 'hr', 'contacts', 'mail',],
+    'data': [
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
